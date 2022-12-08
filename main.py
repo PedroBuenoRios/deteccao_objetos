a68c5133ee78b92579eafa45a7ba7e8d3fe55c56
Pedro Bueno Rios <pedrobuenorios@gmail.com>
2022-12-08 17:36:20 -0300

    Mudanças feitas pelo notebook pessoal

--- main.py
+++ main.py
@@ -4,6 +4,13 @@
 # In[7]:
 
 import os
+import sys
+
+if'--help' in sys.argv:
+    print(  '--data-path\t Caminho para o dataset, o arquivo .yaml deve estar no diretório raiz do dataset\n'\
+            '--batch-size\t Quantas imagens serão carregadas a cada nova época\n'\
+            '--epochs\t Quantas iterações a rede neural irá rodar\n')
+    exit(0)
 
 os.system('pip install torch opencv-python matplotlib')
 
@@ -12,9 +19,11 @@ import glob
 import cv2 as cv
 import matplotlib.pyplot as plt
 import subprocess
+
 TREINAR = True
 GERACOES = 50
 BATCH = 10
+
 
 
 # In[2]:
@@ -46,7 +55,10 @@ def detectar(DIR_RESULTADOS, data_path):
     deteccao_dir_count = len(glob.glob('runs/detect/*'))
     DIR_DETECCAO = f'deteccao_{deteccao_dir_count+1}'
     print(DIR_DETECCAO)
-    subprocess.run(['python3', 'detect.py', '--weights',f'runs/train/{DIR_RESULTADOS}/weights/best.pt','--source', data_path, '--name', DIR_DETECCAO])
+    detect_cmd = [ 'python3', 'detect.py', '--weights',\
+                f'runs/train/{DIR_RESULTADOS}/weights/best.pt',\
+                '--source', data_path, '--name', DIR_DETECCAO]
+    subprocess.run(detect_cmd)
     return DIR_DETECCAO
 
 def visualizar(DIR_DETECCAO):
@@ -60,6 +72,23 @@ def visualizar(DIR_DETECCAO):
         plt.axis('off')
         plt.show()
 
+def treinar(args, batch_max):
+    train_cmd = ['python3', 'train.py','--data', args["data_path"]+'/data.yaml',\
+                    '--weights','yolov5m.pt', '--img', '640', '--epochs',\
+                    f'{args["epochs"]}', '--batch-size', f'{args["batch"]}',\
+                    '--name', args["results_path"], '--cache']
+    try:
+        subprocess.run(train_cmd)
+    except RuntimeError as erro:
+        if 'CUDA out of memory' in erro:
+            torch.cuda.empty_cache()
+            args["batch"] -= 1
+            i = batch_max/args["batch"]
+            for i in range(batch_max):
+                treinar(args, batch_max)
+        else: return False
+    else:
+        return True
 
 # In[3]:
 
@@ -73,9 +102,11 @@ if not os.path.exists('yolov5') or not('yolov5' in os.getcwd()):
     os.chdir('yolov5')
     subprocess.call(['pip3','install', '-r','requirements.txt'])
 
+"""
 rf = Roboflow(api_key="nc0bgygPzfvks88x2Dsv")
 project = rf.workspace("ic-xo5gl").project("dados_rpg")
 dataset = project.version(1).download("yolov5")
+"""
 
 rf = Roboflow(api_key="nc0bgygPzfvks88x2Dsv")
 project = rf.workspace("joseph-nelson").project("mask-wearing")
@@ -83,21 +114,31 @@ dataset = project.version(4).download("yolov5")
 
 
 # In[8]:
+# Recebendo valores passados pelo shell 
+valid_args = {  "data-path": '--data-path',\
+                "batch-size": '--batch-size',\
+                "epochs": '--epochs',\
+                "results-path": '--results_path'}
+args_values = {"data-path": '', "batch-size": 10, "epochs": 100, "results_path": ''}
+try:
+    for index,arg in enumerate(sys.argv):
+        if arg in valid_args:
+            arg_values[arg] = sys.argv[index+1]
+            print(arg)
+except:
+    print('No valid arguments')
 
-
-DIR_RESULTADOS = criar_dir_resultados()
+print(arg_values)
+DIR_RESULTADOS = criar_dir_resultados() if arg_values["results_path"] == '' else arg_values["results_path"]
 os.system('ls')
-subprocess.run(['python3', 'train.py','--data', './Mask-Wearing-4/data.yaml',\
-                '--weights','yolov5m.pt', '--img', '640', '--epochs',\
-                f'{GERACOES}', '--batch-size', f'{BATCH}', '--name', DIR_RESULTADOS, '--cache'])
-
+treinar(arg_values, arg_values["batch-size"])
 
 # In[5]:
 
 
 mostrar_resultados(DIR_RESULTADOS)
 
-IMAGE_INFER_DIR = detectar(DIR_RESULTADOS, './Mask-Wearing-4/test/images')
+IMAGE_INFER_DIR = detectar(DIR_RESULTADOS, args_values["data-path"]+'/test/images')
 visualizar(IMAGE_INFER_DIR)
 
 
