d9fdafa8ce266022c78df4f03a8047ab25229b63
Pedro Bueno Rios <pedrobuenorios@gmail.com>
2023-04-10 09:38:43 -0300

    Arrumando as coisas

--- main.py
+++ main.py
@@ -6,18 +6,11 @@ import argparse
 import yaml
 if __name__ == "__main__":
     torch.cuda.empty_cache()
-    datasetPath = getDatasetFromRoboflow('YOLOv7')
+    # datasetPath = getDatasetFromRoboflow('YOLOv7')
     model = YOLOv7(None)
     with open('../opts.yaml', 'r') as f:
         configs = yaml.load(f, Loader=yaml.SafeLoader)
         for opt, args in configs['yolov7'].items():
             model.train(args)
-
-    infArgs = {
-            'weights': f'./runs/train/exp12/weights/best.pt',
-            'conf': 0.25,
-            'img-size': 480,
-            'source': f'{datasetPath}/test/images/*.jpg'
-            }
 
    # model.detect(infArgs)
--- utils.py
+++ utils.py
@@ -6,48 +6,6 @@ import subprocess
 from roboflow import Roboflow
 import datetime
 
-class YOLOv7_args:
-    def __init__(self):
-        self.train = {
-            'weights' : 'yolov7.pt',
-            'cfg' : '',
-            'data': '',
-            'hyp' : '',
-            'epochs' : 100,
-            'batch-size' : 15,
-            'img-size' : 640,
-            'rect': False,
-            'resume' : False,
-            'nosave' : False,
-            'notest' : False,
-            'noautoanchor' : False,
-            'evolve' : False,
-            'bucket' : '',
-            'cache-images' : True,
-            'image-weights' : False,
-            'device' : '',
-            'multi-scale' : False,
-            'single-cls' : False,
-            'adam' : False,
-            'sync-bn' : False,
-            'local_rank' : -1,
-            'workers' : 8,
-            'project' : './yolov7/runs/train',
-            'entity' : None,
-            'name' : './yolov7/runs/train/exp',
-            'exist-ok': False,
-            'quad' : False,
-            'linar-lr' : False,
-            'label-smoothing' : 0.0,
-            'upload_dataset' : False,
-            'bbox_interval' : -1,
-            'save_period' : -1,
-            'artifact_alias' : 'latest',
-            'freeze' : 0,
-            'v5-metric': False 
-        }
-        self.detect = {}
-
 class Detector:
     def __init__(self, preTrained):
         self.workspace = os.getcwd()
@@ -64,20 +22,6 @@ class Detector:
     def deploy(self):
         pass
 
-    # Retorna o caminho para o melhor peso, o diretório de treinamento precisa ser passado
-    def get_best_weights(self, path:str) -> str:
-        return f'{path}/weights/best.pt'
-
-    # Retorna o último diretório de treinamento
-    def get_last_training_dir(self) -> str:
-        amt = len(glob.glob(f'{self.training_workspace}/exp*'))
-        return f'{self.training_workspace}/exp{amt}'
-
-    # Retorna um diretório novo para treinamento
-    def get_new_training_dir(self) -> str:
-        amt = len(glob.glob(f'{self.training_workspace}/exp*'))
-        return f'{self.training_workspace}/exp{amt+1}'
-
 class YOLOv7(Detector):
     def __init__(self, preTrained:str='yolov7'):
         super().__init__(preTrained)
@@ -87,34 +31,10 @@ class YOLOv7(Detector):
         if not os.path.exists('yolov7'):
             subprocess.run(['git', 'clone', 'https://github.com/WongKinYiu/yolov7'])
             subprocess.run(['pip3', 'install','-r', f'{self.workspace}/yolov7.requirements.txt'])
-        # Realiza o download dos pesos para treinamento se necessário
-        if self.preTrained == None:
-            self.preTrained = '\'\''
-        else:
-            subprocess.run(
-                ['wget', '-nc', '-P',
-                f'{self.workspace}/yolov7/weights', 
-                f'https://github.com/WongKinYiu/yolov7/releases/download/v0.1/{self.preTrained}.pt'])
-            self.preTrained = f'{self.workspace}/yolov7/weights/{self.preTrained}.pt'
         os.chdir('yolov7')
 
-    def train(self, args:dict):
-        # Geração do comando para treinamento
-        cmd = ['python3',  f'train.py']
-        for item, value in args.items():
-            if type(value) != bool:
-                cmd.append(f'--{item}')
-                cmd.append(f'{value}')
-            elif value:
-                cmd.append(f'--{item}')
-        try:
-            subprocess.run(cmd)
-            input()
-        except:
-            pass
-    
-    def detect(self, args: dict):
-        cmd = ['python3', f'detect.py']
+    def callCMDSubprocess(self, arq, args):
+        cmd = ['python', arq]
         for item, value in args.items():
             if type(value) != bool:
                 cmd.append(f'--{item}')
@@ -123,42 +43,43 @@ class YOLOv7(Detector):
                 cmd.append(f'--{item}')
         subprocess.run(cmd)
 
+    def train(self, args:dict):
+        self.callCMDSubprocess('train.py', args)
+    
+    def detect(self, args: dict):
+        self.callCMDSubprocess('detect.py', args)
+
 class YOLOv6(Detector):
     def __init__(self, preTrained='yolov6m'):
         super().__init__(preTrained)
         self.workspace = f'{self.workspace}/yolov6'
         self.training_workspace = f'{self.workspace}/runs/trains/'
         self.detections_workspace = f'{self.workspace}/runs/detections/'
+       
         if not os.path.exists('./yolov6'):
             subprocess.run(['git', 'clone', 'https://github.com/meituan/yolov6'])
             subprocess.run(['pip3', 'install', '-r', f'{self.workspace}/requirements.txt'])
-        if self.preTrained == None:
-            self.preTrained = '\'\''
-        else:
-            subprocess.run(['wget', '-nc', '-P',
-                f'{self.workspace}/weights',
-                f'https://github.com/meituan/YOLOv6/releases/download/0.3.0/{self.preTrained}.pt'])
-            self.preTrained = f'{self.workspace}/weights/{self.preTrained}.pt'
-
-    def train(self, args:dict):
         os.chdir('yolov6')
-        cmd = ['python', f'tools/train.py']
-        for item,value in args.items():
+
+    def callCMDSubprocess(self, arq: str, args: dict):
+        cmd = ['python', arq]
+        for item, value in args.item():
             if type(value) != bool:
                 cmd.append(f'--{item}')
                 cmd.append(f'{value}')
             elif value:
                 cmd.append(f'--{item}')
-        try:
-            subprocess.run(cmd)
-        except:
-            pass
+        subsprocess.run(cmd)
 
-    def detect(self, **kwargs):
-        print(kwargs)
+    def train(self, args:dict):
+        self.callCMDSubprocess('tools/train.py', args)
 
-    def deploy(self, **kwargs):
-        print
+    def detect(self, args: dict):
+        self.callCMDSubprocess('toolo/detect.py', args)
+
+def convertCOCOToYOLO(jsonPath: str):
+    pass
+
 
 def getDatasetFromRoboflow(model):
     folderPath = ''
